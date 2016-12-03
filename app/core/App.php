f1323ba89091a060905c39c6e04954f2123083ba
atdhee.kurteshi@gmail.com <atdhee.kurteshi@gmail.com>
2016-12-03 19:23:01 +0100

    checking if the controllers exist and their methods

--- app/core/App.php
+++ app/core/App.php
@@ -1,13 +1,35 @@
 <?php 
 class App{
-	public $controller="Home";
-	public $method="index";
-	public $paramts=[];
+	public $controller='home';
+	public $method='index';
+	
+	public $params=[];
 	public function __construct(){
-
+	  
+       $url=$this->parsUrl();
+       print_r($url);
+       if(file_exists('/../app/controllers/'.$url[0].'.php'))
+       {
+       	  $this->controller=$url[0];
+       	  unset($url[0]);
+       }
+       require_once '/../app/controllers/'.$this->controller.'.php';
+       $this->controller=new $this->controller;
+       if(isset($url[1]))
+       {
+	       if(method_exists($this->controller,$url[1]))
+	       {
+	       	 echo 'ok';
+	       }
+       }	
+       
+	   
 	}
 	public function parsUrl(){
-		
+		if(isset($_GET['url']))
+		{
+			return $url=explode('/', filter_var(rtrim($_GET['url'],'/'),FILTER_SANITIZE_URL);
+		}
 	}
 }
 
