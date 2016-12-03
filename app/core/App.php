eeafe83bfd646e5ac6b1bfa6d462e18b82d75719
atdhee.kurteshi@gmail.com <atdhee.kurteshi@gmail.com>
2016-12-03 19:27:42 +0100

    adding some commenting to App.php Class

--- app/core/App.php
+++ app/core/App.php
@@ -28,7 +28,7 @@ class App{
 	public function parsUrl(){
 		if(isset($_GET['url']))
 		{
-			return $url=explode('/', filter_var(rtrim($_GET['url'],'/'),FILTER_SANITIZE_URL);
+			return $url=explode('/', filter_var(rtrim($_GET['url'],'/'),FILTER_SANITIZE_URL));
 		}
 	}
 }
